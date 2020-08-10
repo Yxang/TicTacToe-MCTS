f9672ef291382a2c8313d78250767d82363cf81c
Xinyuan Yang <yangxinyuan100@gmail.com>
2020-08-11 07:45:58 +0800

    init commit of TicTacToe Env

---
+++ Env.py
@@ -0,0 +1,111 @@
+import numpy as np
+import copy
+
+class TicTacToe:
+    '''
+        TicTacToe game engine: the goal is to provide a platform for two AI players to play the game in turns and return the game result.
+    '''
+
+    def __init__(self, board=None):
+        ''' Initialize the game state as all zeros (all empty board).  '''
+        if board is None:
+            self.board = np.zeros((3, 3))
+        else:
+            assert isinstance(board, np.ndarray)
+            assert board.shape == (3,3)
+            assert set(np.sort(np.unique(board))).issubset({-1, 0, 1})
+            self.board = board
+        self.state = None
+
+    def get_valid_moves(self, player=None):
+        '''
+           Get a list of available (valid) next moves from a game state of TicTacToe
+            Input:
+                s: the current state of the game, an integer matrix of shape 3 by 3.
+                    s[i,j] = 0 denotes that the i-th row and j-th column is empty
+                    s[i,j] = 1 denotes that the i-th row and j-th column is taken by "X".
+                    s[i,j] = -1 denotes that the i-th row and j-th column is taken by the "O".
+                    For example, the following game state
+                     | X |   | O |
+                     | O | X |   |
+                     | X |   | O |
+                    is represented as the following numpy matrix in game state
+                    s= [[ 1 , 0 ,-1 ],
+                        [-1 , 1 , 0 ],
+                        [ 1 , 0 ,-1 ]]
+            Outputs:
+                m: a list of possible next moves, where each next move is a (r,c) tuple,
+                   r denotes the row number, c denotes the column number.
+            For example, for the following game state,
+                    s= [[ 1 , 0 ,-1 ],
+                        [-1 , 1 , 0 ],
+                        [ 1 , 0 ,-1 ]]
+            the valid moves are the empty grid cells:
+                (r=0,c=1) --- the first row, second column
+                (r=1,c=2) --- the second row, the third column
+                (r=2,c=1) --- the third row , the second column
+            So the list of valid moves is m = [(0,1),(1,2),(2,1)]
+        '''
+        m = [tuple(item) for item in np.argwhere(self.board == 0).tolist()]
+        return m
+
+    def check_game_state(self):
+        '''
+            check if the TicTacToe game has ended or not.
+            If yes (game ended), return the game result (1: x_player win, -1: o_player win, 0: draw)
+            If no (game not ended yet), return None
+
+            Input:
+                s: the current state of the game, an integer matrix of shape 3 by 3.
+                    s[i,j] = 0 denotes that the i-th row and j-th column is empty
+                    s[i,j] = 1 denotes that the i-th row and j-th column is taken by "X" player.
+                    s[i,j] = -1 denotes that the i-th row and j-th column is taken by "O" player.
+            Outputs:
+                e: the result, an integer scalar with value 0, 1 or -1.
+                    if e = None, the game has not ended yet.
+                    if e = 0, the game ended with a draw.
+                    if e = 1, X player won the game.
+                    if e = -1, O player won the game.
+        '''
+        # check the 8 lines in the board to see if the game has ended.
+        a = []
+        a.extend(np.sum(self.board, axis=0).tolist())
+        a.extend(np.sum(self.board, axis=1).tolist())
+        a.append(np.sum([self.board[0, 0], self.board[1, 1], self.board[2, 2]]))
+        a.append(np.sum([self.board[0, 2], self.board[1, 1], self.board[2, 0]]))
+        # if the game has ended, return the game result
+        if 3 in a:
+            e = 1
+        elif -3 in a:
+            e = -1
+        elif 0 in self.board:
+            # if the game has not ended, return None
+            e = None
+        else:
+            e = 0
+        return e
+
+    def __str__(self):
+        map_dict = {0: '  ', 1: 'X ', -1: 'O '}
+        r0 = '|'.join([map_dict[item] for item in self.board[0, :]])
+        r1 = '|'.join([map_dict[item] for item in self.board[1, :]])
+        r2 = '|'.join([map_dict[item] for item in self.board[2, :]])
+        spliter = '\n--------\n'
+        s = r0 + spliter + r1 + spliter + r2
+        return s
+
+    def action(self, a, who):
+        """
+        Do action a on the current game with player who. who=1 represent player X, and who=-1 represent player O
+        :param a: the action, a 2-tuple represents the position of the next step
+        :param who: who is making this action, should be 1 or -1
+        :return g: game after action
+        """
+        assert self.check_game_state() is None  # check the game is not ended
+        assert isinstance(a, tuple) and len(a) == 2  # check if the action is valid
+        assert a[0] in (0, 1, 2) and a[1] in (0, 1, 2)  # check if the action is valid
+        assert self.board[a] == 0  # check if the move is valid
+        assert who in (1, -1)
+        g = copy.deepcopy(self)
+        g.board[a] = who
+        return g
---
+++ __test.py
@@ -0,0 +1,170 @@
+import unittest
+import Env
+import numpy as np
+
+class TestGameEnv(unittest.TestCase):
+    def test_get_valid_moves(self):
+        s = np.array([[1, 0, -1],
+                      [-1, 1, 0],
+                      [1, 0, -1]])
+        game = Env.TicTacToe(s)
+        m = game.get_valid_moves()
+        self.assertTrue(type(m) == list)
+        self.assertTrue(len(m) == 3)
+        self.assertTrue(m[0] == (0, 1) or m[0] == (1, 2) or m[0] == (2, 1))
+        self.assertTrue(m[1] == (0, 1) or m[1] == (1, 2) or m[1] == (2, 1))
+        self.assertTrue(m[2] == (0, 1) or m[2] == (1, 2) or m[2] == (2, 1))
+        self.assertTrue(m[0] != m[1] and m[1] != m[2])
+
+        game = Env.TicTacToe()
+        m = game.get_valid_moves()
+        self.assertTrue(len(m) == 9)
+
+        s = np.array([[1, 0, -1],
+                      [0, 0, 0],
+                      [1, 0, -1]])
+        game = Env.TicTacToe(s)
+        m = game.get_valid_moves()
+        self.assertTrue(len(m) == 5)
+        for i in m:
+            self.assertTrue(i == (0, 1) or i == (1, 0) or i == (1, 1) or i == (1, 2) or i == (2, 1))
+
+    def test_check_game_state(self):
+        s = np.array([[1, 0, 1],
+                      [0, 0, -1],
+                      [0, -1, 0]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e is None)  # the game has not ended yet
+
+        s = np.array([[1, -1, 1],
+                      [0, 1, -1],
+                      [-1, 1, -1]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e is None)  # the game has not ended yet
+
+        s = np.array([[1, 1, 1],
+                      [0, 0, -1],
+                      [0, -1, 0]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[-1, 0, 0],
+                      [1, 1, 1],
+                      [0, -1, 0]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[-1, 0, 0],
+                      [0, 0, -1],
+                      [1, 1, 1]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[1, 0, 0],
+                      [1, 0, -1],
+                      [1, -1, 0]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[-1, 1, 0],
+                      [0, 1, 0],
+                      [-1, 1, 0]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[-1, 0, 1],
+                      [0, 0, 1],
+                      [-1, 0, 1]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[1, 0, 0],
+                      [0, 1, -1],
+                      [-1, 0, 1]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[-1, 0, 1],
+                      [0, 1, 0],
+                      [1, 0, -1]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 1)  # x player wins
+        game = Env.TicTacToe(-s)
+        e = game.check_game_state()
+        self.assertTrue(e == -1)  # O player wins
+
+        s = np.array([[-1, 1, -1],
+                      [1, 1, -1],
+                      [1, -1, 1]])
+        game = Env.TicTacToe(s)
+        e = game.check_game_state()
+        self.assertTrue(e == 0)  # a tiegit
+
+    def test_action(self):
+        s = np.array([[1, 1, 1],
+                      [0, 0, -1],
+                      [0, -1, 0]])
+        game = Env.TicTacToe(s)
+        with self.assertRaises(AssertionError):
+            game.action((1, 0), 1)
+
+        s = np.array([[1, -1, 1],
+                      [-1, 1, -1],
+                      [1, -1, -1]])
+        game = Env.TicTacToe(s)
+        with self.assertRaises(AssertionError):
+            game.action((1, 0), 1)
+
+        s = np.array([[1, 0, 1],
+                      [0, 0, -1],
+                      [0, -1, 0]])
+        game = Env.TicTacToe(s)
+        with self.assertRaises(AssertionError):
+            game.action((0, 1), 2)
+        with self.assertRaises(AssertionError):
+            game.action((0, 2), 1)
+        with self.assertRaises(AssertionError):
+            game.action(0, 1)
+        with self.assertRaises(AssertionError):
+            game.action((0, 3), 2)
+
+        after_game = game.action((0, 1), 1)
+        self.assertTrue(np.alltrue(after_game.board == np.array([[1, 1, 1],
+                                                                 [0, 0, -1],
+                                                                 [0, -1, 0]])))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
